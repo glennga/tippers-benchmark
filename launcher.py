082a5d97d79b83002d342a2399f65056c02d60f3
Glenn <gjgalvizo@gmail.com>
2020-01-21 19:28:46 -0800

    User entry point is the bash "launcher" script, which just wraps the python launcher program and specifies command line arguments.

---
+++ launcher.py
@@ -0,0 +1,6 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run experiments on the tipper\'s benchmark.')
+
+    parser.add_argument('experiment', )
