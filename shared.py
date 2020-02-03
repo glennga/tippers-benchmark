516efe27f09ce6291783210fea0b50c575ba3175
Glenn <gjgalvizo@gmail.com>
2020-02-02 18:27:52 -0800

    Currently have the postgres analyzer working. Next is the mysql analyzer...

--- shared.py
+++ shared.py
@@ -2,21 +2,47 @@
 
 import mysql.connector as mysql
 import psycopg2 as postgres
+import sqlite3
 
 
-def get_mysql_connection(**kwargs):
+def get_mysql_connection(user: str, password: str, host: str):
+    """
+    :param user: Username to use for connection.
+    :param password: Password to use for connection.
+    :param host: Host URI associated with connection.
+    :return: A connection to some MySQL database.
+    """
     return mysql.connect(
-        user=kwargs['user'],
-        passwd=kwargs['passwrd'],
-        host=kwargs['host']
+        user=user,
+        passwd=password,
+        host=host
     )
 
 
-def get_postgres_connection(**kwargs):
+def get_postgres_connection(user: str, password: str, host: str, port: int, database: str):
+    """
+    :param user: Username to use for connection.
+    :param password: Password to use for connection.
+    :param host: Host URI associated with connection.
+    :param port: Port associated with connection.
+    :param database: PostgreSQL database to use upon connecting.
+    :return: A connection to some PostgreSQL database.
+    """
     return postgres.connect(
-        user=kwargs['user'],
-        password=kwargs['password'],
-        host=kwargs['host'],
-        port=kwargs['port'],
-        database=kwargs['database']
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database
+    )
+
+
+def get_results_connection(results_file: str):
+    """
+    :param results_file: File to create / append to.
+    :return: A connection to some SQLite database.
+    """
+    return sqlite3.connect(
+        results_file,
+        check_same_thread=False
     )
